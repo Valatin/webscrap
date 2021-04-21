b5c236fb5b704f569000def123bb822330041e1f
Valatin <valentin.chavatte@gmail.com>
2021-04-21 14:40:36 +0200

    Scrap infos and print them

--- mairie.py
+++ mairie.py
@@ -13,8 +13,8 @@ def main():
         sys.exit("Usage: mairie.py datafile.csv")
 
     # Initialize variables and tmp we need later   
-    dicterror = {
-        "mairie": "Not Found",
+    dict_error = {
+        "maire": "Not Found",
         "telephone": "Not Found",
         "horaires": "Not Found"
     }
@@ -29,19 +29,51 @@ def main():
             # Append the dict row to the mairies array
             mairies.append(row)
 
+    # Iterate through all cities
     for city in mairies:
-        print(city["url"])
+        # Try to connect to webpage
         try:
-            page = requests.get(city["url"], timeout=10)
-            if not page.status_code == requests.codes.ok:
-                city.update(dicterror)
-                print("Error")
-            else:
-                print(page.status_code)
-        except requests.exceptions.ReadTimeout:
-            print("TimeOut")
+            page = requests.get(city["url"], timeout=5)
+        
+        # Skip if request give error code
+        except requests.exceptions.RequestException:
+            city.update(dict_error)
+            print("TimeOut or page not found")
 
-    print(mairies)
+        # Skip if cannot connect to url
+        if not page.status_code == requests.codes.ok:
+            city.update(dict_error)
+            print("Error: URL Not Found")
+
+        # Proceed to look for info
+        else:
+            soup=bs(page.content, 'html.parser')
+            
+            # Checks if good url
+            if soup.title.text == "Erreur!":
+                city.update(dict_error)
+                print("Error: Wrong URL")
+            
+            # Else proceeds with scraping
+            else :
+                infosoup = soup.find("div", {"id": "mairie"})
+
+                # Search for mayor: (Monsieur ou madame) (a word) (- or " ") (a word with an Uppercase) (1 or 0 word with uppercase)
+                maire = re.search('(Monsieur |Madame )\w+[-\ ]([A-Z][a-z]+ )*[A-Z]+( [A-Z]+)*', str(infosoup))
+                city["maire"] = "Mayor Not Found" if maire == None else maire.group()
+
+                # Search for phone nbr
+                telephone = re.search('((\d{2}) ){4}(\d{2})', str(infosoup))
+                city["telephone"] = "Phone Not Found" if telephone == None else telephone.group()
+
+                # Search for opening hours
+                horairessoup = soup.find('h2', text = re.compile('Horaires')).next_sibling
+                horaires = " ".join(horairessoup.text.split())
+                city["horaires"] = horaires.split('ouverte : ')[1]
+
+        print("Status: {}, {}, Mayor:{}, Tel: {}, Horaires: {}".format(page.status_code, city["commune"], city["maire"], city["telephone"], city["horaires"]))
+
+
     
     print("--- {} seconds ---".format(time.time() - start_time))
 
